cdf74ced04f04349f310dfd833ca87be062302e1
Ashish Mishra <ashish.mishra@powerbooks.in>
2018-12-01 20:27:36 +0530

    testing

---
+++ flaskBlog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'ABC',
+        'title': 'C++',
+        'content': 'First post',
+        'pdate': 'Nov-28-2018'
+    },
+    {
+        'author': 'XYZ',
+        'title': 'Python',
+        'content': 'Second post',
+        'pdate': 'Nov-30-2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
